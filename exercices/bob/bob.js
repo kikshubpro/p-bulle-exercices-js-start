dfe6773827d76ee227550683ab871ac949e04bae
Kilian Schopfer <pb20dsi@eduvaud.ch>
2026-01-07 18:54:41 +0100

    feat(bob) : réaliser tous les objectifs de l'exercice
    [30][done]

--- exercices/bob/bob.js
+++ exercices/bob/bob.js
@@ -4,5 +4,15 @@
 //
 
 export const hey = (message) => {
-  throw new Error('Remove this line and implement the function');
+  if (message.replace(/\s/g, "") == "") {
+    return "Fine. Be that way!"
+  } else if (message.endsWith("?") && /[a-zA-Z]/g.test(message) && message.replace(/[0-9]/g, "") == message.replace(/[0-9]/g, "").toUpperCase()) {
+    return "Calm down, I know what I'm doing!"
+  } else if (message.replace(/\s/g, "").endsWith("?")) {
+    return "Sure."
+  } else if (/[a-zA-Z]/g.test(message) && message.replace(/[0-9]/g, "") == message.replace(/[0-9]/g, "").toUpperCase()) {
+    return "Whoa, chill out!"
+  } else {
+    return "Whatever."
+  }
 };
