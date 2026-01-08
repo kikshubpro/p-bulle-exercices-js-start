f5c5869abeeed548373b97837c8b31c0b9fdc745
Kilian Schopfer <pb20dsi@eduvaud.ch>
2026-01-08 20:05:29 +0100

    feat(book-store) : terminer l'exercice après avoir revu plusieurs fois la manière de faire
    [120][done]

--- exercices/book-store/book-store.js
+++ exercices/book-store/book-store.js
@@ -4,5 +4,47 @@
 //
 
 export const cost = (books) => {
-  throw new Error('Remove this line and implement the function');
+  const count = [0, 0, 0, 0, 0]
+
+  books.forEach(book => {
+    count[book - 1]++
+  });
+
+  const groups = []
+
+  while (count.some(c => c > 0)) {
+    let groupSize = 0
+
+    for (let i = 0; i < 5; i++) {
+      if (count[i] > 0) {
+        count[i]--
+        groupSize++
+      }
+    }
+
+    groups.push(groupSize)
+  }
+
+  while (groups.includes(3) && groups.includes(5)) {
+    groups.splice(groups.indexOf(5), 1)
+    groups.splice(groups.indexOf(3), 1)
+
+    groups.push(4, 4)
+  }
+
+  let totalCost = 0
+
+  const discounts = {
+    1 : 0,
+    2 : 0.05,
+    3 : 0.1,
+    4 : 0.2,
+    5 : 0.25
+  }
+
+  groups.forEach(group => {
+    totalCost += 800 * group * (1 - discounts[group])
+  });
+
+  return totalCost
 };
